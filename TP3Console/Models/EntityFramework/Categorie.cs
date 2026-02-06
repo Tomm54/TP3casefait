251f797475fdcdb11851fd55b91214c9caf7da91
Xznoow <antoinedegeorgescoc@gmail.com>
2026-02-06 14:33:56 +0100

    Ajoutez des fichiers projet.

---
+++ TP3Console/Models/EntityFramework/Categorie.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TP3Console.Models.EntityFramework;
+
+[Table("categorie")]
+public partial class Categorie
+{
+    
+
+
+    [Key]
+    [Column("idcategorie")]
+    public int Idcategorie { get; set; }
+
+    [Column("nom")]
+    [StringLength(50)]
+    public string Nom { get; set; } = null!;
+
+    [Column("description")]
+    public string? Description { get; set; }
+
+    [InverseProperty("IdcategorieNavigation")]
+    public virtual ICollection<Film> Films { get; set; } = new List<Film>();
+
+
+
+    private ILazyLoader _lazyLoader;
+    public Categorie(ILazyLoader lazyLoader)
+    {
+        _lazyLoader = lazyLoader;
+    }
+
+    private ICollection<Film> films;
+
+    [InverseProperty("CategorieNavigation")]
+    public virtual ICollection<Film> Films
+    {
+        get
+        {
+            return _lazyLoader.Load(this, ref films);
+        }
+        set { films = value; }
+    }
+}
---
+++ TP3Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using TP3Console.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP3Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new FilmsDbContext())
+            {
+                //Chargement de la catégorie Action
+                Categorie categorieAction = ctx.Categories.First(c => c.Nom == "Action");
+                Console.WriteLine("Categorie : " + categorieAction.Nom);
+                Console.WriteLine("Films : ");
+                //Chargement des films de la catégorie Action.
+                foreach (var film in categorieAction.Films) // lazy loading initiated
+                {
+                    Console.WriteLine(film.Nom);
+                }
+            }
+
+        }
+    }
+}
